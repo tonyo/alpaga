fb1ac7bdf1168e7d4853c4f12e3de79ddca7aa4c
Anton Ovchinnikov <anton@tonyo.info>
2018-07-04 11:48:15 +0200

    release: 0.1.2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'alpaga',
+    version = '0.1.2',
+    url = 'https://github.com/tonyo/alpaga/',
+    author = 'Anton Ovchinnikov',
+    author_email = 'test-pypi-alpaga@tonyo.info',
+    description = 'test package',
+    packages = find_packages(),    
+    install_requires = [],
+)
